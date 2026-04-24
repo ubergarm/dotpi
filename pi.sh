e5e180f5ac2921717bbe6b42d24eac0d39368f27
John W. Leimgruber III <leimgrub@gmail.com>
2026-04-24 17:47:13 -0400

    fixup pi.sh to not change dir

--- pi.sh
+++ pi.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-cd "$SCRIPT_DIR"
 
 export PI_CODING_AGENT_DIR="${SCRIPT_DIR}/.pi"
 node '/app/pi/node_modules/@mariozechner/pi-coding-agent/dist/cli.js'
